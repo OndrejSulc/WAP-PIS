3cf4356cece3a0f6a3f9353bd81b0029c13890e0
Ondrej Sulc <ondrej.sulc.1@email.cz>
2022-04-13 22:57:23 +0200

    added getAllUsers to webclient userManagement.js

--- wwwroot/webclient/js/userManagement.js
+++ wwwroot/webclient/js/userManagement.js
@@ -52,3 +52,21 @@ async function deleteUser(UserID) {
     });
     return await response.json();
 }
+
+
+async function getAllUsers() {
+
+  const response = await fetch("/UserManagement/GetAllUsers", {
+    method: 'GET', 
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow', 
+    referrerPolicy: 'no-referrer', 
+ 
+  });
+  return await response.json();
+}
