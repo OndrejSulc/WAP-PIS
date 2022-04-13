7da40fa1ca076f0739052e70a2b0515079827ac2
Ondrej Sulc <ondrej.sulc.1@email.cz>
2022-04-13 21:36:01 +0200

    cleaning login.js on webclient

--- wwwroot/webclient/js/login.js
+++ wwwroot/webclient/js/login.js
@@ -1,35 +1,23 @@
-function createLoginJSON(login, pw){
-    return {
-    "Login": login,
-    "Password": pw,
-    "Successful_Authentication": null,
-    "IsCEO": null
-    }
-};
-
-async function Login()
-{
-    var login = document.getElementById("login_login").value;
-    var pw = document.getElementById("login_pw").value;
-    var json = createLoginJSON(login,pw);
-    var response = await postLogin(json);
-
-    document.getElementById("result").innerHTML += JSON.stringify(response)+"<br>";
-}
-
-async function CheckLogin()
+async function checkLogin()
 {
     var response = await fetch("/Authentication/CheckLogin");
     document.getElementById("result").innerHTML += await response.text()+"<br>";
 }
 
-async function Logout()
+async function logout()
 {
     var response = await fetch("/Authentication/Logout",{method:'POST'});
     document.getElementById("result").innerHTML += await response.text() +"<br>";
 }
 
-async function postLogin(data) {
+async function login(login, pw) {
+
+    var body={
+        "Login": login,
+        "Password": pw,
+        "Successful_Authentication": null,
+        "IsCEO": null
+        }
 
     const response = await fetch("/Authentication/Login", {
       method: 'POST', 
@@ -41,7 +29,8 @@ async function postLogin(data) {
       },
       redirect: 'follow', 
       referrerPolicy: 'no-referrer', 
-      body: JSON.stringify(data) 
+      body: JSON.stringify(body) 
     });
+
     return await response.json();
 }
