3e8519181a9b2e6c0b6aa597b42b64d753c5904e
xgladi00 <48547408+xgladi00@users.noreply.github.com>
2022-04-26 16:03:02 +0200

    Update console log on signalr

--- wwwroot/webclient/js/signalr.js
+++ wwwroot/webclient/js/signalr.js
@@ -2,15 +2,15 @@ async function startSignalR(onNotification) {
     const connection = new signalR.HubConnectionBuilder()
         .withUrl("/notificationHub")
         .withAutomaticReconnect()
-        .configureLogging(signalR.LogLevel.Information)
+        .configureLogging(signalR.LogLevel.Warning)
         .build();
     connection.on("NotificationAdded", message => {
         onNotification(JSON.parse(message))
     })
     try {
         await connection.start();
-        console.log("SignalR Connected");
+        //console.log("SignalR Connected");
     } catch (err) {
-        console.log(err);
+        //console.log(err);
     }
 }
